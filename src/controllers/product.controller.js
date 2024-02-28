90825d8b65d2c6ccef62d2318ff6797ed1f6d717
Facundo Caamaño <facundoadriancaamano@outlook.com>
2024-02-27 21:56:50 -0300

    editar productos

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose"
 import productModel from "../models/products.model.js"
 
 
@@ -15,6 +16,59 @@ export const getProductById = async(req,res)=>{
     const id = req.params.id
     const product = await productModel.findById(id)
     res.json(product)
+}
+
+export const getPublishedProducts = async(req,res) =>{
+    const userId = req.params.userId
+    const products = await productModel.find({sellerId:userId})
+
+    res.json(products)
+}
+
+export const deleteProduct = async (req,res)=>{
+    try {
+        // Obtiene el ID del producto de los parámetros de la solicitud
+        const productId = req.params.productId;
+
+        // Elimina el producto utilizando el método del modelo
+        const result = await productModel.deleteOne({ _id: productId });
+
+        // Comprueba si se encontró y eliminó un producto
+        if (result.deletedCount === 1) {
+            // Si se eliminó correctamente, envía una respuesta JSON
+            return res.json({ message: 'Producto eliminado' });
+        } else {
+            // Si no se encontró el producto, devuelve un mensaje de error
+            return res.status(404).json({ error: 'Producto no encontrado' });
+        }
+    } catch (error) {
+        // Si ocurre algún error durante el proceso, devuelve un mensaje de error
+        console.error('Error al eliminar el producto:', error);
+        return res.status(500).json({ error: 'Se produjo un error al eliminar el producto' });
+    }
+}
+
+export const editProduct = async(req,res)=>{
+    const productId = req.params.productId
+    const {title,price,description,code,stock} = req.body
+    //const product = await productModel.findById(productId)
+
+    const product = {
+        title,
+        price,
+        description,
+        code,
+        stock
+    }
+    try{
+        await productModel.findByIdAndUpdate(productId, product)
+        res.json({message: 'producto editado'})
+
+    }catch(error){
+        console.error('Error al editar el producto:', error);
+        res.status(500).json({ error: 'Se produjo un error al editar el producto' });
+    }
+    
 }
 
 export const stock = async (productId, quantity, res) => {
--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { createProduct, getProductById, getProducts } from '../controllers/product.controller.js'
+import { createProduct, deleteProduct, editProduct, getProductById, getProducts, getPublishedProducts } from '../controllers/product.controller.js'
 import { checkToken } from '../../utils.js'
 
 const router = express.Router()
 
 router.get('/products', getProducts)
 router.get('/products/:id',getProductById)
+router.get('/products/my-products/:userId', getPublishedProducts)
+router.put('/products/edit-my-products/:productId', editProduct)
 router.post('/products', createProduct)
-
+router.delete('/products/delete-product/:productId', deleteProduct)
 export default router
